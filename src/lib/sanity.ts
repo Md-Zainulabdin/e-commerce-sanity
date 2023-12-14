fdd22c286bcffb8d52513df9f2458afc84f77070
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-14 21:48:07 +0500

    build a connection btw frontend & sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-const client = createClient({
+export const client = createClient({
   projectId: "j3ojy73k",
   dataset: "production",
   apiVersion: "2022-03-25",
