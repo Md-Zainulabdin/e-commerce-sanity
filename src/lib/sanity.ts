906c0fed65e0c5265ca82e34d8088efdde6341b2
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-17 23:38:10 +0500

    refactor code

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,8 @@
+// Import necessary modules from next-sanity and @sanity/image-url
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
+// Create a Sanity client with the specified configuration
 export const client = createClient({
   projectId: "j3ojy73k",
   dataset: "production",
@@ -8,8 +10,10 @@ export const client = createClient({
   useCdn: true,
 });
 
+// Create an image URL builder using the Sanity client
 const builder = imageUrlBuilder(client);
 
+// Function to generate image URLs using the builder
 export function urlFor(source: any) {
   return builder.image(source);
 }
